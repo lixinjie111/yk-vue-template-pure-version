2b829d06d53384e671f94d675c60e40444d6e7b8
shilu <shilu@tusvn.comm>
2019-09-07 17:40:27 +0800

    11

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -15,7 +15,7 @@
                             <el-input type="text" v-model.trim="loginForm.userNo" :maxlength="40" placeholder="请输入用户名"></el-input>
                         </el-form-item>
                         <el-form-item prop="password" label="密码" class="login-item">
-                            <el-input type="password" v-model.trim="loginForm.password" :maxlength="20" placeholder="请输入密码" @click.enter.native="handleLogin"></el-input>
+                            <el-input type="password" v-model.trim="loginForm.password" :maxlength="20" placeholder="请输入密码" @keyup.enter.native="handleLogin"></el-input>
                         </el-form-item>
                     </el-form>
                     <el-button class="login-button" type="primary" :loading="loading" @click.native.prevent="handleLogin">登 录</el-button>
