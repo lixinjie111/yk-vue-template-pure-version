672c459fc01cef97b5851034f45e8fa55f9b8a05
shilu <shilu@tusvn.comm>
2019-12-26 14:48:05 +0800

    优化

--- static/config/config.js
+++ static/config/config.js
@@ -8,5 +8,5 @@ window.config = {
     staticUrl: "http://120.133.21.14:9090/", //静态文件路径
 	url: "http://120.133.21.14:9090/operateApp/",  	//平台地址 运营平台 登录，登出，修改密码使用
 
-	version: 1.0       // 版本号
+	version: "1.0"       // 版本号
 };
