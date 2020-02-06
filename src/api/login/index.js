def27498bfe5e4d50f91a37c7a8de469901dca4b
shilu <shilu@tusvn.com>
2020-02-06 18:32:34 +0800

    删除备注

--- src/api/login/index.js
+++ src/api/login/index.js
@@ -21,17 +21,3 @@ export const requestCreateCode = params => { return axios.post(`${HTTPURL}openAp
  * 滑动校验
  */
 export const requestAuthCode = params => { return axios.post(`${HTTPURL}openApi/user/authCode`, params).then(res => res.data); };
-
-
-/**
- * 登录
- */
-// export const requestLogin = params => { return axios.post(`${HTTPURL}openApi/v2/user/login`, params).then(res => res.data); };
-/**
- * 退出登录
- */
-// export const requestLogout = params => { return axios.post(`${HTTPURL}openApi/v2/user/logout`, params).then(res => res.data); };
-/**
- * 修改密码
- */
-// export const requestPasswd = params => { return axios.post(`${HTTPURL}openApi/v2/user/passwd`, params).then(res => res.data); };
