1260ec6313ee7e3d73ef47e847829264898a752e
shilu <shilu@tusvn.com>
2020-02-20 00:40:38 +0800

    element-ui 提取外链

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-// import Vue from 'vue';  //加载优化
 import App from './App';
 import router from './router';
 import store from './store';
-
-// Element-ui
-// import ElementUI from 'element-ui';  //加载优化
-// import 'element-ui/lib/theme-chalk/index.css';  //加载优化
-// Vue.use(ElementUI);  //加载优化
 
 // 进度条
 import NProgress from 'nprogress';
@@ -24,23 +16,11 @@ import '@/assets/scss/reset.scss';
 import '@/assets/scss/public.scss';
 import '@/assets/scss/element-ui-reset.scss';
 import '@/assets/icon-font/iconfont.css';
-// import '@/assets/icon-font/iconfont.js';  //不需要
-
-// 时间转换--组件内各自引用
-// import TDate from '@/assets/js/utils/date.js'  //加载优化
-// Vue.prototype.$dateUtil = TDate;  //加载优化
 
 // 权限
 import { setAuthInfo, getAdminId, getAuthInfo, removeAuthInfo } from '@/session/index';
 // 在免登录白名单，直接进入
 const whiteList = ['/login','/404'];
-// setAuthInfo({
-//     userNo: "********",
-//     id: "********"
-// });
-// removeAuthInfo();
-// router global config
-
 
 //取消请求的对象
 window.cancleSource={};
